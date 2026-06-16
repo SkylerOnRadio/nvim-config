6fccf6e91f7dbed45df5d063419ecd487669d10e
SkylerOnRadio <abhigyadulal@gmail.com>
2026-06-16 16:42:16 +0530

    added inlay hints

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -42,4 +42,12 @@ return {
 			})
 		end,
 	},
+	{
+		"MysticalDevil/inlay-hints.nvim",
+		event = "LspAttach",
+		dependencies = { "neovim/nvim-lspconfig" },
+		config = function()
+			require("inlay-hints").setup()
+		end,
+	},
 }

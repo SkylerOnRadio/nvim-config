fc7a5ea24d360b8f0cbc53d3ec570faacb481852
SkylerOnRadio <abhigyadulal@gmail.com>
2026-06-17 15:06:50 +0530

    made indenting like lazyvim, and made the yank highlight coulor using catppuccin mauve

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,10 +1,20 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("catppuccin")
-    end,
-  },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha",
+				custom_highlights = function(colors)
+					return {
+						-- Creating a unique highlight group just for yanking
+						YankFlash = { bg = colors.mauve, fg = colors.crust },
+					}
+				end,
+			})
+
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
 }
--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -1,31 +1,72 @@
 return {
+	-- 1. Use indent-blankline for the quiet, static background lines
 	{
 		"lukas-reineke/indent-blankline.nvim",
 		main = "ibl",
 		config = function()
-			local highlight = { "RainbowDelimiter" }
-
-			local hooks = require("ibl.hooks")
-
-			hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-				vim.api.nvim_set_hl(0, "RainbowDelimiter", { fg = "#cba6f7" }) -- catppuccin mauve
-			end)
-
 			require("ibl").setup({
 				indent = {
 					char = "│",
-					highlight = { "IblIndent" }, -- dim color for normal indent lines
+					highlight = { "IblIndent" },
 				},
 				scope = {
-					enabled = true,
-					highlight = highlight, -- bright color for current scope
-					show_start = false, -- underline on scope open line
-					show_end = true, -- underline on scope close line
+					enabled = false, -- Disable IBL's static scope so mini.indentscope can take over
 				},
 			})
+		end,
+	},
 
-			-- scope requires treesitter to detect the current block
-			hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+	-- 2. Use mini.indentscope configured EXACTLY like LazyVim
+	{
+		"nvim-mini/mini.indentscope",
+		version = false,
+		config = function()
+			require("mini.indentscope").setup({
+				draw = {
+					delay = 100,
+					predicate = function(scope)
+						return not scope.body.is_incomplete
+					end,
+					priority = 2,
+				},
+				mappings = {
+					object_scope = "ii",
+					object_scope_with_border = "ai",
+					goto_top = "[i",
+					goto_bottom = "]i",
+				},
+				options = {
+					border = "both",
+					indent_at_cursor = true,
+					n_lines = 10000,
+					try_as_border = false,
+				},
+				symbol = "│",
+			})
+
+			-- LazyVim Autocommand: Disable indentscope in specific filetypes (Dashboard, Help, Terminal, etc.)
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = {
+					"alpha",
+					"dashboard",
+					"fzf",
+					"help",
+					"lazy",
+					"lazyterm",
+					"mason",
+					"neo-tree",
+					"notify",
+					"toggleterm",
+					"Trouble",
+					"trouble",
+				},
+				callback = function()
+					vim.b.miniindentscope_disable = true
+				end,
+			})
+
+			-- Set the active scope color to Catppuccin Mauve (#cba6f7)
+			vim.api.nvim_set_hl(0, "MiniIndentscopeSymbol", { fg = "#cba6fe", nocombine = true })
 		end,
 	},
 }

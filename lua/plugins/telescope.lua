a6feee8aa9bc35c8bb19334d996a686dbfd75347
SkylerOnRadio <abhigyadulal@gmail.com>
2026-06-18 10:12:02 +0530

    Merged projects.lua and telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,31 +1,42 @@
--- telescope.lua
 return {
+	-- 1. Project Management Core
+	{
+		"ahmedkhalf/project.nvim",
+		config = function()
+			require("project_nvim").setup({
+				manual_mode = false,
+				detection_methods = { "pattern" },
+				patterns = { ".git", "Makefile", "package.json" },
+				silent_chdir = false,
+				scope_chdir = "global",
+				datapath = vim.fn.stdpath("data"),
+			})
+		end,
+	},
+
+	-- 2. Telescope Configuration
 	{
 		"nvim-telescope/telescope.nvim",
 		version = "*",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
-			-- Add fzf-native for massive performance gains in sorting
 			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+			"nvim-telescope/telescope-ui-select.nvim",
+			"ahmedkhalf/project.nvim",
 		},
 		config = function()
 			local builtin = require("telescope.builtin")
 
-			-- Your original keymaps
+			-- Global Keymaps
 			vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find Files" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
-
-			-- New QoL keymaps
 			vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "Grep word under cursor" })
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find open buffers" })
 			vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Find recent files" })
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find help tags" })
 			vim.keymap.set("n", "<leader>resume", builtin.resume, { desc = "Resume last search" })
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
+
+			-- Setup Telescope with Extension Mappings
 			require("telescope").setup({
 				extensions = {
 					["ui-select"] = {
@@ -37,12 +48,57 @@ return {
 						override_file_sorter = true,
 						case_mode = "smart_case",
 					},
+					projects = {
+						-- Intercepting the selection inside the project list
+						mappings = {
+							i = {
+								["<CR>"] = function(prompt_bufnr)
+									local project_actions = require("telescope._extensions.project.actions")
+									local has_persistence, persistence = pcall(require, "persistence")
+
+									if has_persistence then
+										-- 1. Save and pause current project's session tracking
+										persistence.stop()
+									end
+
+									-- 2. Wipe current active buffers so old project context doesn't spill over
+									vim.cmd("silent! %bd")
+
+									-- 3. Let project.nvim execute the directory change
+									project_actions.change_working_directory(prompt_bufnr, "global")
+
+									if has_persistence then
+										-- 4. Automatically locate and pull up the target project's session file
+										persistence.load()
+									end
+								end,
+							},
+							n = {
+								["<CR>"] = function(prompt_bufnr)
+									local project_actions = require("telescope._extensions.project.actions")
+									local has_persistence, persistence = pcall(require, "persistence")
+
+									if has_persistence then
+										persistence.stop()
+									end
+									vim.cmd("silent! %bd")
+									project_actions.change_working_directory(prompt_bufnr, "global")
+									if has_persistence then
+										persistence.load()
+									end
+								end,
+							},
+						},
+					},
 				},
 			})
-			require("telescope").load_extension("ui-select")
-			require("telescope").load_extension("fzf") -- Load the fzf extension
 
-			-- Your project keymap
+			-- Load extensions
+			require("telescope").load_extension("ui-select")
+			require("telescope").load_extension("fzf")
+			require("telescope").load_extension("projects")
+
+			-- Project keymap pointing directly to the picker
 			vim.keymap.set("n", "<leader>fp", "<cmd>Telescope projects<CR>", { desc = "Find Projects" })
 		end,
 	},

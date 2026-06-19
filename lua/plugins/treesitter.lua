fcca03a59db438280c11c26118707b3f4b18b681
SkylerOnRadio <abhigyadulal@gmail.com>
2026-06-19 09:58:22 +0530

    added treesitter objects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -79,4 +79,12 @@ return {
 			})
 		end,
 	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main",
+		init = function()
+			vim.g.no_plugin_maps = true
+		end,
+		config = function() end,
+	},
 }
